af719dc2741a2bd3955b84e9b88003f9de581e6e
Innova Engineers <innovaengineeringtech@gmail.com>
2017-09-03 22:06:21 +0530

    Created Spring Boot Intro module 1. Display hello world in jsp using Spring Boot

---
+++ spring-boot-intro/src/main/java/com/innova/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.innova.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Nirupam on 02-09-2017.
+ */
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ spring-boot-intro/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Nirupam
+  Date: 02-09-2017
+  Time: 18:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring Boot Intro</title>
+</head>
+<body>
+Hello World
+</body>
+</html>
