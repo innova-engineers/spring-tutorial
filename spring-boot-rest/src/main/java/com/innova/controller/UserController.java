e204c7a6e64d1ee12fded29b9b789e0f9bb4dc10
Innova Engineers <innovaengineeringtech@gmail.com>
2017-09-04 19:14:07 +0530

    Created Spring Boot Rest module 1. A hello world RESTful services using Spring Boot

---
+++ spring-boot-rest/src/main/java/com/innova/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.innova.controller;
+
+import com.innova.beans.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Nirupam on 03-09-2017.
+ */
+@RestController
+@RequestMapping("/rest/user")
+public class UserController {
+
+    private final String HELLO_TEMPLATE = "Hello %s";
+
+    @RequestMapping(
+            value = "/hello",
+            method = RequestMethod.GET,
+            produces = "application/json"
+    )
+    public User sayHello(@RequestParam(value = "name") String name){
+        return new User(String.format(HELLO_TEMPLATE, name));
+    }
+}
