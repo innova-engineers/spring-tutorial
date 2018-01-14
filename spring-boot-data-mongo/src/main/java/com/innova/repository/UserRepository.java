fbd6be64cecb9fb257b71119f0d01127dd563ad6
Innova Engineers <innovaengineeringtech@gmail.com>
2018-01-14 12:34:05 +0530

    Added using spring data mongodb cloud with spring boot

---
+++ spring-boot-data-mongo/src/main/java/com/innova/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.innova.repository;
+
+import com.innova.model.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<Users, String> {
+}
